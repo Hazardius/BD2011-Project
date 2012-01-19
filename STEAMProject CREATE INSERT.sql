4927579229602a3fb294751ff26b511e1253593e
Marcin Hanc <marcin@hanc>
2012-01-19 18:17:45 +0100

    Issue #4 zrobiony.

--- STEAMProject CREATE INSERT.sql
+++ STEAMProject CREATE INSERT.sql
@@ -10,7 +10,8 @@ GO
 SET LANGUAGE polski
 GO
 
---------- CREATE
+--------- CREATE (Utworzenie podstawy bazy danych, czyli tabel
+--------- i kilku zale¿noœci miêdzy nimi)
 
 create table SteamWallet
 (id int not null primary key,
@@ -80,96 +81,59 @@ nazwa varchar(50) not null,
 opis varchar(100),
 idProd int references Produkty(id) not null);
 
+create table OsiagnieciaOdblokowane
+(id int not null primary key,
+osiagniecie int references Osiagniecia(id),
+kolekcjoner int references Klienci(steamid));
+
+create table Posiadania
+(id int not null primary key,
+produkt int references Produkty(id),
+wlasciciel int references Klienci(steamid));
+
+create table ObiektyNaWishlist
+(id int not null primary key,
+produkt int references Produkty(id),
+autorWishlisty int references Klienci(steamid),
+priorytet int not null,
+check (priorytet > -1));
+
+create table PozycjeTransakcji
+(id int not null primary key,
+produkt int references Produkty(id),
+transakcja int references Transakcje(id));
+
 GO
 
----------- INSERT
+---------- TU BÊD¥ FUNKCJE DODAJ¥CE I USUWAJ¥CE
 
+--nic na razie nie ma :(
 
-insert into Stanowiska
+---------- INSERT (Dodanie kilku przyk³adowych wartoœci na pocz¹tek)
+
+insert into Produkty
 values ('profesor', 3000, 5000);
-insert into Stanowiska
-values ('adiunkt', 2000, 3000);
-insert into Stanowiska
-values ('doktorant', 900, 1300);
-insert into Stanowiska
-values ('sekretarka', 1500, 2500);
-insert into Stanowiska
-values ('techniczny', 1500, 2500);
-insert into Stanowiska
-values ('dziekan', 2700, 4800);
-
 
 insert into Pracownicy
 values(1, 'Wachowiak', null, 4500, 900, 'profesor', '01-09-1980');
-insert into Pracownicy
-values(2, 'Jankowski', 1, 2500, null, 'adiunkt', '01-09-1990');
-insert into Pracownicy
-values(3, 'Fio³kowska', 1, 2550, null, 'adiunkt', '01-01-1985');
-insert into Pracownicy
-values(4, 'Mielcarz', 1, 4000, 400, 'profesor', '01-12-1980');
-insert into Pracownicy
-values(5, 'Ró¿ycka', 4, 2800, 200, 'profesor', '01-09-2001');
-insert into Pracownicy
-values(6, 'Miko³ajski', 4, 1000, null, 'doktorant', '01-10-2002');
-insert into Pracownicy
-values(7, 'Wójcicki', 5, 1350, null, 'doktorant', '01-10-2003');
-insert into Pracownicy
-values(8, 'Listkiewicz', 1, 2200, null, 'sekretarka', '01-09-1980');
-insert into Pracownicy
-values(9, 'Wróbel', 1, 1900, 300, 'techniczny', '01-01-1999');
-insert into Pracownicy
-values(10, 'Andrzejewicz', 5, 2900, null, 'adiunkt', '01-01-2002');
 
+------------ SELECT (Pokazanie zawartoœci naszej bazy)
+------------ Na razie za pomoc¹ selectów. PóŸniej wykorzystamy funkcje!
 
-insert into Projekty
-values('e-learning','01-01-2009', '31-05-2010', null, 5, 100);
-insert into Projekty
-values('web service', '10-11-1999', '31-12-2000', '20-04-2001', 4, 90)
-insert into Projekty
-values('semantic web', '01-10-2007', '01-09-2009', null, 4, 85)
-insert into Projekty
-values('neural network', '01-01-1998', '30-06-2000', '30-06-2000', 1, 120)
-
-
-insert into Realizacje
-values(10, 5, 8)
-insert into Realizacje
-values(10, 10, 6)
-insert into Realizacje
-values(10, 9, 2)
-
-insert into Realizacje
-values(20, 4, 8)
-insert into Realizacje
-values(20, 6, 8)
-insert into Realizacje
-values(20, 9, 2)
-
-insert into Realizacje
-values(30, 4, 8)
-insert into Realizacje
-values(30, 6, 6)
-insert into Realizacje
-values(30, 10, 6)
-insert into Realizacje
-values(30, 9, 2)
-
-insert into Realizacje
-values(40, 1, 8)
-insert into Realizacje
-values(40, 2, 4)
-insert into Realizacje
-values(40, 3, 4)
-insert into Realizacje
-values(40, 9, 2)
-
-
-
------------- SELECT
-
-select * from Stanowiska
-select * from Pracownicy
-select * from Projekty
-select * from Realizacje
-
-
+select * from Czlonkostwa
+select * from DLC
+select * from Grupy
+select * from Gry
+select * from Klienci
+select * from ObiektyNaWishlist
+select * from Osiagniecia
+select * from OsiagnieciaOdblokowane
+select * from OST
+select * from Posiadania
+select * from PozycjeTransakcji
+select * from Produkty
+select * from SDK
+select * from SteamWallet
+select * from Transakcje
+select * from Utwory
+select * from Znajomosci
