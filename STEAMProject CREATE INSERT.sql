eedf91904a1ad995b7508a38c8852b8242b3856d
Hazardius <s362625@.labs.wmi.amu.edu.pl>
2012-01-20 11:29:01 +0100

    Procedury dodajace

--- STEAMProject CREATE INSERT.sql
+++ STEAMProject CREATE INSERT.sql
@@ -20,14 +20,15 @@ kwota money default 0);
 create table Klienci
 (steamid int not null primary key,
 nazwa_wyswietlana varchar(40) not null,
-haslo varchar(64),
+haslo varchar(64) not null,
 data_urodzenia datetime,
 portfel int references SteamWallet(id) unique,
 check (Len(haslo) = 64));
 
 create table Znajomosci
-(znajomy1 int references Klienci(steamid),
-znajomy2 int references Klienci(steamid));
+(znajomy1 int references Klienci(steamid) not null,
+znajomy2 int references Klienci(steamid) not null,
+check (znajomy1 != znajomy2));
 
 create table Transakcje
 (id int not null primary key,
@@ -59,7 +60,7 @@ create table Utwory
 autor varchar(40),
 dlugosc int,
 wielkosc int not null,
-album int references OST(id),
+album int references OST(id) not null,
 check (wielkosc > 0),
 check (dlugosc > 0));
 
@@ -69,7 +70,7 @@ ost int references OST(id));
 
 create table DLC
 (id int references Produkty(id) primary key,
-gra int references Gry(id),
+gra int references Gry(id) not null,
 ost int references OST(id));
 
 create table SDK
@@ -83,14 +84,12 @@ opis varchar(100),
 idProd int references Produkty(id) not null);
 
 create table OsiagnieciaOdblokowane
-(id int not null primary key,
-osiagniecie int references Osiagniecia(id),
-kolekcjoner int references Klienci(steamid));
+(osiagniecie int references Osiagniecia(id) not null,
+kolekcjoner int references Klienci(steamid) not null);
 
 create table Posiadania
-(id int not null primary key,
-produkt int references Produkty(id),
-wlasciciel int references Klienci(steamid));
+(produkt int references Produkty(id) not null,
+wlasciciel int references Klienci(steamid) not null);
 
 create table ObiektyNaWishlist
 (id int not null primary key,
@@ -162,13 +161,17 @@ begin try
         set @cena = 0
     if (@wielkosc is null)
         raiserror ('Nie podano wielkosci pliku!', 11, 2)
+    if (@tytulPierwszegoUtworu is null)
+        raiserror ('Nie podano nazwy pierwszego utworu!', 11, 3)
+    if (@wielkoscSciezki is null)
+        raiserror ('Nie podano wielkosci utworu!', 11, 4)
     if exists (select * from Produkty where (nazwa = @nazwa))
     begin
-       raiserror ('Istnieje już produkt o tej nazwie!', 11, 3)
+       raiserror ('Istnieje już produkt o tej nazwie!', 11, 4)
     end
     if exists (select * from Utwory where (tytul = @tytulPierwszegoUtworu))
     begin
-       raiserror ('Istnieje już utwor o tej nazwie!', 11, 4)
+       raiserror ('Istnieje już utwor o tej nazwie!', 11, 5)
     end
     else
     begin
@@ -221,6 +224,85 @@ begin try
         values (@idek, @nazwa, @cena, @wielkosc, @opis)
         insert into Gry(id, ost)
         values (@idek, @ost)
+    end
+end try
+begin catch
+               SELECT ERROR_NUMBER() AS 'NUMER BLEDU',ERROR_MESSAGE() AS 'KOMUNIKAT'
+end catch
+GO
+
+create procedure dodaj_DLC
+       @nazwa varchar(40),
+       @cena money,
+       @wielkosc int,
+       @opis varchar(400),
+       @gra int,
+       @ost int
+AS
+begin try
+    if (@nazwa is null)
+        raiserror ('Nie podano nazwy!', 11, 1)
+    if (@cena is null)
+        set @cena = 0
+    if (@wielkosc is null)
+        raiserror ('Nie podano wielkosci pliku!', 11, 2)
+    if (@gra is null)
+        raiserror ('Nie podano gry!', 11, 3)
+    if exists (select * from Produkty where (nazwa = @nazwa))
+    begin
+       raiserror ('Istnieje już produkt o tej nazwie!', 11, 4)
+    end
+    if not exists (select * from Produkty where (id = @gra))
+    begin
+       raiserror ('Nie istnieje taka gra!', 11, 5)
+    end
+    else
+    begin
+        declare @idek int
+        set @idek = RAND(@wielkosc)
+        while exists (select * from Produkty where (id = @idek))
+        begin
+            set @idek = RAND(@wielkosc)
+        end
+        insert into Produkty(id, nazwa, cena, wielkosc, opis)
+        values (@idek, @nazwa, @cena, @wielkosc, @opis)
+        insert into DLC(id, gra, ost)
+        values (@idek, @gra, @ost)
+    end
+end try
+begin catch
+               SELECT ERROR_NUMBER() AS 'NUMER BLEDU',ERROR_MESSAGE() AS 'KOMUNIKAT'
+end catch
+GO
+
+create procedure dodaj_Utwor
+       @tytulPierwszegoUtworu varchar(40),
+       @autor varchar(40),
+       @dlugosc int,
+       @wielkoscSciezki int,
+       @album int
+AS
+begin try
+    if (@tytulPierwszegoUtworu is null)
+        raiserror ('Nie podano tytułu pierwszego utworu!', 11, 1)
+    if (@autor is null)
+        raiserror ('Nie podano autora!', 11, 2)
+    if (@wielkoscSciezki is null)
+        raiserror ('Nie podano wielkosci pliku!', 11, 3)
+    if (@album is null)
+        raiserror ('Nie podano albumu!', 11, 4)
+    if not exists (select * from OST where (id = @album))
+    begin
+       raiserror ('Istnieje już produkt o tej nazwie!', 11, 5)
+    end
+    if exists (select * from Utwory where (tytul = @tytulPierwszegoUtworu))
+    begin
+       raiserror ('Istnieje już utwor o tej nazwie!', 11, 5)
+    end
+    else
+    begin
+        insert into Utwory(tytul, autor, dlugosc, wielkosc, album)
+        values (@tytulPierwszegoUtworu, @autor, @dlugosc, @wielkoscSciezki, @album)
     end
 end try
 begin catch
